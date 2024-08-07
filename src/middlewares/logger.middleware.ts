c17a65062b2cf4c8729e673ec8297aa5971ba782
Khaled AbdElRahim <khaled710017@gmail.com>
2024-08-07 18:27:16 +0300

    Add request logs

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const bodyData: string = JSON.stringify(req.body) ?? 'no body';
+
+    console.log(`${req.method} ${req.path} - ${bodyData}`); // Log method, path, and body
+    next(); // Pass control to the next middleware function
+};
+
+
+export default loggerMiddleware;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { config } from "dotenv";
+import Logs from "./config/logs";
+import loggerMiddleware from "./middlewares/logger.middleware"
 
 // load env variables
 config();
@@ -7,10 +9,13 @@ config();
 const server: Express = express();
 const port: number = process.env.SERVER_PORT || 4000;
 
+server.use(loggerMiddleware);
+
+
 server.get("/", (req: Request, res: Response): void => {
   res.send("Hello World!");
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  Logs.info(`Server is running on port ${port}`);
 });
