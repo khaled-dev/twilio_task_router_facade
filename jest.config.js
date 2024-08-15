4a1a299b0c6f6aa8af43f53ff654a46412a7351f
Khaled AbdElRahim <khaled710017@gmail.com>
2024-08-16 02:43:38 +0300

    Remove unneeded test configs

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
-  testTimeout: 60000,
-  forceExit: true,
   globals: {
     "ts-jest": {
       tsconfig: "tsconfig.jest.json",
